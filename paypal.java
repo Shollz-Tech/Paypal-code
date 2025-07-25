e5d4c4184a7512b89a5c7d6452ad60caeb5ae309
Augustine <ogunladeshollz007@gmail.com>
2025-07-25 19:55:57 +0000

    modified

--- paypal.java
+++ paypal.java
@@ -1,1 +1,4 @@
 Git is a type Ditributed Version Control System (DVCS) where every commit is a snapshot of the system at a given point in time. The strength of Git is its local versioning ability where every work done could be saved in local machine until it is ready to share or push it somewhere else. Git allows teams to collaborate efficiently. Git's Rollback ability is another reason why its used in my environment i.e  if a deployment fails, we can quickly revert to a stable version.
+
+
+Please read the above statements carefully.
